11ad8cd320e80f42d720f0e38e0833c671249917
zhidev <Doug@douglass-macbook-air.local>
2016-03-08 20:02:08 -0800

    Test

--- Venatus/AppDelegate.swift
+++ Venatus/AppDelegate.swift
@@ -22,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        //print("test")
+        
         // Override point for customization after application launch.
         return true
     }
